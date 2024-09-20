30acff697b68a737698a75c587e2114f0eaca207
yogesh7833 <yogeshkus783@gmail.com>
2024-09-20 18:33:06 +0530

    starting frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-700 text-2xl font-bold'>App</div>
+  )
+}
+
+export default App
---
+++ frontend/src/router/router.jsx
@@ -0,0 +1,10 @@
+const { createBrowserRouter } = require("react-router-dom");
+
+const router=createBrowserRouter([
+    {
+        path:'/',
+        element:<div>Hello world</div>
+    },
+])
+
+export default router;
